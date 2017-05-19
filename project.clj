213cb3514333b31656e398167fcafcc3421d9de4
Klavs Taube <klavs@fullcontact.com>
2017-05-19 19:32:38 +0300

    revert nippy version to 2.10.0

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.jodah/expiringmap "0.4.1"]
                  [net.spy/spymemcached "2.12.2"]
-                 [com.taoensso/nippy "2.13.0"]
+                 [com.taoensso/nippy "2.10.0"]
                  [fullcontact/full.core "0.10.1"
                   :exclusions [org.clojure/clojurescript]]
                  [fullcontact/full.async "0.9.0"]]
